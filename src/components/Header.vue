89579b66e2b52e30797f431bbd81d306f5c4444d
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2021-08-03 19:26:26 +0900

    Fix: #2 Headerコンポーネントを作成

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="d-flex justify-start mb-6">
+    <router-link to="/" class="mr-5 grey--text">Home</router-link>
+    <router-link to="/about" class="mr-5 grey--text">About</router-link>
+    <router-link to="/works" class="mr-5 grey--text">Works</router-link>
+    <router-link to="/contact" class="mr-5 grey--text">Contact</router-link>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  font-size: large;
+}
+</style>
