73a9eb121f8ca4909573a7c6c6b047e632881525
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2021-08-04 11:28:14 +0900

    Fix: #4 ホーム画面作成

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <p>this is Home page</p>
+  <v-container class="pb-10">
+    <v-row class="d-flex justify-end">
+      <h1 class="font-italic font-weight-thin text-sm-h1">
+        yaak
+        <br>
+        portfolio
+        <br>
+        site
+      </h1>
+    </v-row>
+    <v-row class="d-flex justify-start">
+      <v-img
+          :src="require('../../assets/dog.png')"
+      />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
