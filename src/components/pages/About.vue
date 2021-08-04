a1f223c3906ea9f5dbe02b08a7edacd1c7a13018
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2021-08-04 12:03:57 +0900

    Fix: #9 Aboutページ作成

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>this is About page</p>
+  <v-container>
+    <v-list>
+      <v-list-item class="text-sm-h4">経歴</v-list-item>
+      <v-list-item class="text-sm-body-1 ml-10">- hoge hoge</v-list-item>
+      <v-list-item class="text-sm-body-1 ml-10">- fuga fuga</v-list-item>
+      <v-list-item class="text-sm-h4">技術</v-list-item>
+      <v-list-item class="text-sm-body-1 ml-10">- hoge hoge</v-list-item>
+      <v-list-item class="text-sm-body-1 ml-10">- fuga fuga</v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
