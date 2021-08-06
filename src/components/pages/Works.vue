f0b2c8376ed3baea35f6a50de780f36400fac2ae
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2021-08-06 15:21:14 +0900

    Fix: #10 work画面を実装

--- src/components/pages/Works.vue
+++ src/components/pages/Works.vue
@@ -1,10 +1,42 @@
 <template>
-  <p>this is Works page</p>
+  <v-container>
+    <p class="text-sm-h5">担当した業務</p>
+    <works-card
+        title="hoge hoge"
+        date="fuga fugaa"
+        content="fugo fugo"
+        class="mb-10"
+    ></works-card>
+    <works-card
+        title="hoge hoge"
+        date="fuga fugaa"
+        content="fugo fugo"
+        class="mb-10"
+    ></works-card>
+    <works-card
+        title="hoge hoge"
+        date="fuga fugaa"
+        content="fugo fugo"
+        class="mb-10"
+    ></works-card>
+    <works-card-with-img
+        title="this is title"
+        imageSrc="dog.png"
+        imageAlt="犬の画像"
+        year="2021-02-02"
+        comment="this is comment area this is comment area this is comment area this is comment area this is comment area this is comment area this is comment area"
+        url="https://github.com/akiyoshiyamamoto">
+    </works-card-with-img>
+  </v-container>
 </template>
 
 <script>
+import WorksCard from "@/components/WorksCard";
+import WorksCardWithImg from "@/components/WorksCardWithImg";
+
 export default {
-name: "Works"
+  name: "Works",
+  components: {WorksCardWithImg, WorksCard},
 }
 </script>
 
